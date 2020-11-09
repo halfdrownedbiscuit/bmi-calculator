8ba6a891259e8370bb849bb0fa701442745ae56e
halfdrownedbiscuit <halfdrownedbiscuit@gmail.com>
2020-11-09 20:26:18 +0530

    style change + error message

--- src/components/Calculator/Result/Result.tsx
+++ src/components/Calculator/Result/Result.tsx
@@ -7,7 +7,8 @@ import {
   _isMobile,
   _isResultOpen,
   _weightKg,
-  _weightUnits
+  _weightUnits,
+  _gender
 } from 'store/atoms';
 import utils from 'utils';
 
@@ -21,6 +22,7 @@ const Result = () => {
   const weightUnits = useRecoilValue(_weightUnits);
   const heightM = useRecoilValue(_heightM);
   const weightKg = useRecoilValue(_weightKg);
+  const hasGender = useRecoilValue(_gender) != null;
   const hasHeight = heightM > 0;
   const hasWeight = weightKg > 0;
 
@@ -35,7 +37,13 @@ const Result = () => {
     !hasHeight ||
     !hasWeight
   ) {
-    return <Nothing />;
+    return (
+      <Nothing
+        hasHeight={hasHeight}
+        hasWeight={hasWeight}
+        hasGender={hasGender}
+      />
+    );
   }
   return (
     <div className={styles.Result}>
@@ -90,14 +98,77 @@ const Result = () => {
   );
 };
 
-const Nothing = () => {
+const Nothing = ({
+  hasHeight,
+  hasWeight,
+  hasGender
+}: {
+  hasHeight: boolean;
+  hasWeight: boolean;
+  hasGender: boolean;
+}) => {
+  if (hasGender && hasHeight && hasWeight) {
+    return (
+      <div className={styles.Nothing}>
+        <div className={styles.FillSection}>Ready to Calculate</div>
+      </div>
+    );
+  }
   return (
     <div className={styles.Nothing}>
       <div className={styles.FillSection}>
-        Fill Gender, Height and Weight then click Calculate
+        {calcFillMessage(hasGender, hasHeight, hasWeight)}
       </div>
     </div>
   );
+};
+
+const calcFillMessage = (
+  hasGender: boolean,
+  hasHeight: boolean,
+  hasWeight: boolean
+) => {
+  // hack to get no. of fill in message fields
+  let count = Number(!hasGender) + Number(!hasHeight) + Number(!hasWeight);
+  let fields;
+  if (count == 3) {
+    fields = (
+      <>
+        <span className={styles.ToFill}>gender</span>
+        {', '}
+        <span className={styles.ToFill}>height</span>
+        {' and '}
+        <span className={styles.ToFill}>weight</span>{' '}
+      </>
+    );
+  } else if (count == 1) {
+    fields = (
+      <>
+        {!hasGender ? <span className={styles.ToFill}>gender</span> : null}
+        {!hasWeight ? <span className={styles.ToFill}>weight</span> : null}
+        {!hasHeight ? <span className={styles.ToFill}>height</span> : null}{' '}
+      </>
+    );
+  } else {
+    let errs = [];
+    if (!hasGender) {
+      errs.push('gender');
+    }
+    if (!hasHeight) {
+      errs.push('height');
+    }
+    if (!hasWeight) {
+      errs.push('weight');
+    }
+    fields = (
+      <>
+        <span className={styles.ToFill}>{errs[0]}</span>
+        {' and '}
+        <span className={styles.ToFill}>{errs[1]}</span>{' '}
+      </>
+    );
+  }
+  return <>Please fill {fields} to calculate BMI</>;
 };
 
 export default Result;
